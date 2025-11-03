37df20bf1818486e7e2f21bd05ab2ae17343fd79
Ayan-css <ayan48311@gmail.com>
2025-11-03 10:48:33 +0530

    cors configuration

--- project-management/src/app.js
+++ project-management/src/app.js
@@ -1,6 +1,20 @@
 import express from "express"
-
+import cors from "cors"
 const app = express()
+
+// Basic configurations
+app.use(express.json({ limit: "16kb"}))
+app.use(express.urlencoded({ extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+
+// cors configuration
+app.use(cors({
+    origin: process.env.CORS_ORIGIN?.split(",")|| "https://localhost:5173",
+    credentials:true,
+    methods: ["GET","POST","PUT","PACTH","DELETE","OPTIONS"],
+    allowedHeaders: ["Content-Type","Authorization"]
+}))
+
 
 app.get("/",(req, res) =>{
     res.send("welcome to basecampy")
