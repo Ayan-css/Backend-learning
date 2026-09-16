ec9b7c4a3fbcae7a722e0f265dee7696aca4167b
Ansari Mohd Ayan Nasiruddin <ayan48311@gmail.com>
2026-09-16 11:30:18 +0530

    sfd

--- day-3/src/db/db.js
+++ day-3/src/db/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
 async function connectDB() {
-    await mongoose.connect("mongodb+srv://yt:Ve9CnpskL8YQgKsQ@backend-database.yu7aqeb.mongodb.net/halley")
+    await mongoose.connect("mongodb+srv://yt:Ve9CnpskL8YQgfdhsfdggfdfaKsQ@backeneaswefd-database.yu7aqeb.mongodb.net/halley")
 
     console.log("Connected to DB");
     
